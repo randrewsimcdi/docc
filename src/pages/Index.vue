fe05fe45ceb49fbd0e6c5f723f7d1dd082bb8e24
randrewsimcdi <69466106+randrewsimcdi@users.noreply.github.com>
2020-09-23 09:08:45 -0400

    style: homepage products, juniper upsell

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -32,34 +32,41 @@
       <div class="flex flex-wrap justify-center -mx-3">
       
         <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/4">
-                   <g-image src="../assets/pharos-crm.png" />
+                 <g-link to="/docs/" class="ml-auto font-bold leading-none text-white transition-all duration-200 ease-out transform hover:shadow-xl hover:-translate-y-1">  <g-image src="../assets/pharos-crm.png" />
           <h3 class="font-bold tracking-wide uppercase text-ui-primary">Pharos CRM  </h3>
            <div class="flex justify-center mt-8">
         </div>
-
+</g-link>
 
         </div>
 
 
          <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/4">
+                   <g-link to="/docs/" class="ml-auto font-bold leading-none text-white transition-all duration-200 ease-out transform hover:shadow-xl hover:-translate-y-1">
                    <g-image src="../assets/product-fastlane.png" />
+                  
           <h3 class="font-bold tracking-wide uppercase text-ui-primary">Product FastLane  </h3>
            <div class="flex justify-center mt-8">
         </div>
+        </g-link>
         </div>
 
          <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/4">
+                   <g-link to="/docs/" class="ml-auto font-bold leading-none text-white transition-all duration-200 ease-out transform hover:shadow-xl hover:-translate-y-1">
                    <g-image src="../assets/pharos-sales-app.png" />
           <h3 class="font-bold tracking-wide uppercase text-ui-primary">Pharos Spotlight  </h3>
            <div class="flex justify-center mt-8">
         </div>
+                   </g-link>
         </div>
 
          <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/4">
+                   <g-link to="/docs/" class="ml-auto font-bold leading-none text-white transition-all duration-200 ease-out transform hover:shadow-xl hover:-translate-y-1">
                    <g-image src="../assets/pharos-web.png" />
           <h3 class="font-bold tracking-wide uppercase text-ui-primary">Pharos Web  </h3>
            <div class="flex justify-center mt-8">
         </div>
+                   </g-link>
         </div>
 
 
@@ -69,7 +76,18 @@
 
       </div> 
 
+      <div class="bg-green-400 text-center py-4 lg:px-4">
+        <g-link to="https://www.junipercommerce.com/">
+  <div class="p-2 bg-green-400 items-center text-green-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+    <span class="font-semibold mr-2 text-left flex-auto">With the launch of Juniper, IMC now offers a complete, omnichannel approach to growing and managing your business</span>
+      <span class="flex rounded-full bg-green-600 uppercase px-2 py-1 text-xs font-bold mr-3">Learn More</span>
+
+  </div>
+  </g-link>
+</div>
+
     </div>
+    
   </Layout>
 </template>
 
