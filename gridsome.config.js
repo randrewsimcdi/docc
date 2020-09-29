14ee9fa063865d190be4a0c375f4a4af005a8897
randrewsimcdi <69466106+randrewsimcdi@users.noreply.github.com>
2020-09-29 17:09:58 -0400

    update: Deployed Google Analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -84,7 +84,7 @@ module.exports = {
     {
       use: '@gridsome/plugin-google-analytics',
       options: {
-        id: (process.env.GA_ID ? process.env.GA_ID : 'XX-999999999-9')
+        id: (process.env.GA_ID ? process.env.GA_ID : 'UA-179270162-1')
       }
     },
 
