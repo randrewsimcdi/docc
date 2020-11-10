8450bffef62251467f884e16fdaf4ca4b55eec4b
randrewsimcdi <69466106+randrewsimcdi@users.noreply.github.com>
2020-11-09 21:21:10 -0500

    v2 missed files

---
+++ src/pages/releasenotes.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout>
+    <div class="pt-8 md:pt-16">
+      <h1 class="text-center font-bold text-4xl text-ui-primary">Release Notes</h1>
+      <ul class="text-center">
+        <li>Note 1</li>
+        
+      </ul>
+    </div>
+    <div class="mb-10 flex items-center justify-center">
+      <g-link to="/" class="font-bold border-b border-ui-primary text-xl">Go back</g-link>.
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Release Notes'
+  }
+}
+</script>
