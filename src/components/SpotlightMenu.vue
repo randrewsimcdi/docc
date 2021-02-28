bd2cbfbbebf7e1f910141e57ffe935e6f35d34b7
randrewsimcdi <69466106+randrewsimcdi@users.noreply.github.com>
2021-02-28 16:45:06 -0500

    Spotlight User Guide

--- src/components/SpotlightMenu.vue
+++ src/components/SpotlightMenu.vue
@@ -3,7 +3,8 @@
             
             <a href="https://drive.google.com/file/d/10Wy4zI5RRodv6L7u1b_oadDkMnQWTghW/view?usp=sharing"><button class="mr-6 bg-ui-primary hover:bg-blue-400 text-white font-bold py-2 my-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">🏆 Best Practices</button></a>
             <a href=""><button class="mr-6 bg-ui-primary hover:bg-blue-400 text-white font-bold py-2 my-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">⚠️ Common Issues (Coming Soon)</button></a>
-            <a href="https://apps.apple.com/us/app/pharos-sales/id1399174841#?platform=ipad"><button class="mr-6 bg-ui-primary hover:bg-blue-400 text-white font-bold py-2 my-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">📲 Download App</button></a>
+            <a href="https://apps.apple.com/us/app/pharos-sales/id1399174841#?platform=ipad"><button class="mr-6 bg-ui-primary hover:bg-blue-400 text-white font-bold py-2 my-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">📲 Download Latest App</button></a>
+            <a href="https://drive.google.com/file/d/1X2FzNcoMR6noCNNE6MmUBcLQHnWP4OWb/view?usp=sharing"><button class="mr-6 bg-ui-primary hover:bg-blue-400 text-white font-bold py-2 my-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">📙 User Guide</button></a>
 
 
        </div>
